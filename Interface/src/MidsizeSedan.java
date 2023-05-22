eb782af2f42d93d4919d4bef1de4a35a2a4cb442
shayrin0 <shayrin.panahi@gmail.com>
2023-05-22 18:23:51 -0400

    practice code for Interface

---
+++ Interface/src/MidsizeSedan.java
@@ -0,0 +1,39 @@
+public class MidsizeSedan implements Vehicle {
+
+    /* the @Override annotation indicates that the methods in our class (MidsizeSedan) were
+    defined in the interface (Vehicle) and this class is providing an implementation. */
+    @Override
+    public void moveForward(int milesPerHour) {
+        // implementation code here...
+    }
+
+    @Override
+    public void moveBackward(int milesPerHour) {
+        // implementation code here...
+    }
+
+    @Override
+    public void stop() {
+        // implementation code here...
+    }
+
+    @Override
+    public void turnLeft() {
+        // implementation code here...
+    }
+
+    @Override
+    public void turnRight() {
+        // implementation code here...
+    }
+
+    @Override
+    public void engineOn() {
+        // implementation code here...
+    }
+
+    @Override
+    public void engineOff() {
+        // implementation code here...
+    }
+}
